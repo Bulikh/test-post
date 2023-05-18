bb26881032a17bd4d9fb386c819f1bd8101685a8
Guram Khubulava <guram_khubulava@epam.com>
2023-05-18 13:27:09 +0400

    Added postMessage handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import Cookies from 'js-cookie';
 import './App.css';
 
+enum MessageType {
+  updateCookie = 'UPDATE_COOKIE'
+}
+
+interface IData {
+  type: MessageType;
+  domain: string;
+  email?: string;
+  shipping?: {
+    company?: string;
+    firstName?: string;
+    lastName?: string;
+    address?: string;
+    unit?: string;
+    city?: string;
+    state?: string;
+    zip?: string;
+    country?: string;
+    phone?: string;
+    alternatePhone?: string;
+  };
+  billing?: {
+    company?: string;
+    firstName?: string;
+    lastName?: string;
+    address?: string;
+    unit?: string;
+    city?: string;
+    state?: string;
+    zip?: string;
+    country?: string;
+    phone?: string;
+    alternatePhone?: string;
+  }
+}
+
 function App() {
+  useEffect(() => {
+    console.log('iframe loaded');
+    const postMessageCallback = function (e: MessageEvent<IData>) {
+      console.log('got post message', e);
+      const { type, domain, ...rest } = e.data;
+      if (type === MessageType.updateCookie) {
+        console.log('set cookies');
+        Cookies.set('customerPrefill', `${encodeURIComponent(JSON.stringify({
+          ...rest,
+        }))};`, {
+          domain,
+          path: '/',
+          secure: true,
+          sameSite: 'None'
+        })
+        console.log('get cookies', Cookies.get('customerPrefill'));
+      }
+    }
+    window.addEventListener('message', postMessageCallback, false);
+    return () => window.removeEventListener('message', postMessageCallback, false);
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test cookies</h1>
     </div>
   );
 }
